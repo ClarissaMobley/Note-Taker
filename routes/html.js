ce9f0498a6340534b9cd1cc3bec7aa537e58a7d5
Clarissa Mobley <clarissamobley@me.com>
2024-05-15 18:22:55 -0400

    added comments

--- routes/html.js
+++ routes/html.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const path = require("path");
 
 
-// Get Notes
+// Get Note.html
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
-// Get HTML
+// Get Index.html
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
-
+// Export router
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const htmlRoutes = require("./html");
 const apiRoutes = require("./api");
 
+// Middleware routing
 router.use("/api", apiRoutes);
 router.use("/", htmlRoutes);
 
-
+// Export router
 module.exports = router
