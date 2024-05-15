a8a3e993c1b058f6a88abf9c354710407636fd0b
Clarissa Mobley <clarissamobley@me.com>
2024-05-15 11:35:45 -0400

    added a new folder

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
