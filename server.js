86f388903a7eeee49a167811740a4e9742101d10
Clarissa Mobley <clarissamobley@me.com>
2024-05-15 11:44:00 -0400

    made changes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/apiRoutes', require('./routes/apiRoutes'));
 
 const PORT = process.env.PORT || 3001;
 
